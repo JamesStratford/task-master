acec4fe531ea5024b258499c5a87f0ab0abf835a
JamesStratford <james.e.stratford@gmail.com>
2023-08-24 09:28:28 +1200

    Added discord crate widget, refactored code. Now exports two functions, widget and crate.

--- frontend/src/components/DiscordWidget.jsx
+++ frontend/src/components/DiscordWidget.jsx
@@ -1,41 +1,37 @@
 import WidgetBot from '@widgetbot/react-embed'
 import React, { useEffect } from 'react'
 
-const DiscordWidgetCrate = () => {
+const DiscordWidgetCrate = React.memo(function DiscordWidgetCrate(props) {
     useEffect(() => {
-      // Load the external script
-      const script = document.createElement('script');
-      script.src = 'https://cdn.jsdelivr.net/npm/@widgetbot/crate@3';
-      script.async = true;
-      
-      script.onload = () => {
-        // Once the script is loaded, initialize the Crate object
-        const crate = new window.Crate({
-          server: '1133857547305111592',
-          channel: '1133857547816808530'
-        });
-  
-        crate.notify('Test notification');
-        
-        crate.on('signIn', data => {
-          console.log(`Guest signed in as ${data.name}`);
-          crate.emit('sendMessage', 'Hello world');
-        });
-      };
-  
-      // Append the script to the document
-      document.body.appendChild(script);
-  
-      // Cleanup the script when the component unmounts
-      return () => {
-        document.body.removeChild(script);
-      };
-    }, []);
-  
-    return null;
-}
+        const script = document.createElement('script');
+        script.src = 'https://cdn.jsdelivr.net/npm/@widgetbot/crate@3';
+        script.async = true;
 
-export default function DiscordWidget(props) {
+        script.onload = () => {
+            // Once the script is loaded, initialize the Crate object
+            const crate = new window.Crate({
+                server: props.server,
+                channel: props.channel
+            });
+
+            crate.on('signIn', data => {
+                console.log(`Guest signed in as ${data.name}`);
+            });
+        };
+
+        // Append the script to the document
+        document.body.appendChild(script);
+
+        // Cleanup the script when the component unmounts
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    return null;
+})
+
+const DiscordWidget = React.memo(function DiscordWidget(props) {
     return (
         <div>
             <WidgetBot
@@ -44,9 +40,11 @@ export default function DiscordWidget(props) {
                 server={props.server}
                 channel={props.channel}
             />
-            <DiscordWidgetCrate />
         </div>
     )
-}
+})
+
+export { DiscordWidget, DiscordWidgetCrate };
+
 
 

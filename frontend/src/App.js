23a9a68d242157b08b6b6432fd607630fb701614
JamesStratford <james.e.stratford@gmail.com>
2023-08-24 16:17:17 +1200

    Switched to react-beautiful-dnd Needs implementation https://github.com/atlassian/react-beautiful-dnd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
         </TabList>
 
         <TabPanel className={activeTabIndex === 0 ? 'tab-panel active' : 'tab-panel'}>
-          <KanbanBoard/>
+        <KanbanBoard/>
        </TabPanel>
         <TabPanel className={activeTabIndex === 1 ? 'tab-panel active' : 'tab-panel'}>
           <DiscordWidget server='1133857547305111592' channel='1133857547816808530' />
--- frontend/src/components/KanbanBoard.jsx
+++ frontend/src/components/KanbanBoard.jsx
@@ -1,25 +1,11 @@
-import { KanbanComponent, ColumnsDirective, ColumnDirective } from "@syncfusion/ej2-react-kanban";
-import React, { useEffect } from 'react'
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-let data = [
-  { Id: 1, Status: 'Open', Summary: 'Analyze the new requirements gathered from the customer.', Type: 'Story', Priority: 'Low', Tags: 'Analyze,Customer', Estimate: 3.5, Assignee: 'Nancy Davloio', RankId: 1 },
-  { Id: 2, Status: 'InProgress', Summary: 'Fix the issues reported in the IE browser.', Type: 'Bug', Priority: 'Release Breaker', Tags: 'IE', Estimate: 2.5, Assignee: 'Janet Leverling', RankId: 2 },
-  { Id: 3, Status: 'Testing', Summary: 'Fix the issues reported by the customer.', Type: 'Bug', Priority: 'Low', Tags: 'Customer', Estimate: '3.5', Assignee: 'Steven walker', RankId: 1 },
-  { Id: 4, Status: 'Close', Summary: 'Arrange a web meeting with the customer to get the login page requirements.', Type: 'Others', Priority: 'Low', Tags: 'Meeting', Estimate: 2, Assignee: 'Michael Suyama', RankId: 1 },
-  { Id: 5, Status: 'Validate', Summary: 'Validate new requirements', Type: 'Improvement', Priority: 'Low', Tags: 'Validation', Estimate: 1.5, Assignee: 'Robert King', RankId: 1 }
-];
 
-const KanbanBoard = () => { // Define the KanbanBoard component as a functional component
+
+const KanbanBoard = () => {
   return (
-    <div className="Kanban">
-      <KanbanComponent id="kanban" keyField="Status" dataSource={data} cardSettings={{ contentField: "Summary", headerField: "Id" }}>
-        <ColumnsDirective>
-          <ColumnDirective headerText="To Do" keyField="Open" />
-          <ColumnDirective headerText="In Progress" keyField="InProgress" />
-          <ColumnDirective headerText="Testing" keyField="Testing" />
-          <ColumnDirective headerText="Done" keyField="Close" />
-        </ColumnsDirective>
-      </KanbanComponent>
+    <div>
+      <DragDropContext/>
     </div>
   );
 };

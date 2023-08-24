0541de6fc681b139df6633bd811eea6458ed6f05
JamesStratford <james.e.stratford@gmail.com>
2023-08-24 20:45:39 +1200

    tidy up app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,22 @@ import './App.css';
 import { DiscordWidget, DiscordWidgetCrate } from './components/DiscordWidget';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-import React, { useState } from 'react';
+import React from 'react';
 
 
 function App() {
-  const [activeTabIndex, setActiveTabIndex] = useState(0);
-
   return (
     <div className="App">
-      <Tabs onSelect={index => setActiveTabIndex(index)}>
+      <Tabs>
         <TabList>
           <Tab>Title 1</Tab>
           <Tab>Discord</Tab>
         </TabList>
 
-        <TabPanel className={activeTabIndex === 0 ? 'tab-panel active' : 'tab-panel'}>
+        <TabPanel>
           <h2>Any content 1</h2>
         </TabPanel>
-        <TabPanel className={activeTabIndex === 1 ? 'tab-panel active' : 'tab-panel'}>
+        <TabPanel>
           <DiscordWidget server='1133857547305111592' channel='1133857547816808530' />
         </TabPanel>
       </Tabs>

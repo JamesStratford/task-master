21f797966021050f0fed8f9b682821e799d8ace9
JamesStratford <james.e.stratford@gmail.com>
2023-09-13 22:47:27 +1200

    Added whitelist error feedback to login, and DEBUG mode feature to add a user ID to the whitelist

--- frontend/src/components/DiscordOAuth.jsx
+++ frontend/src/components/DiscordOAuth.jsx
@@ -7,6 +7,7 @@ const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 const DiscordAuth = (props) => {
     const [user, setUser] = useState(null);
+    const [hasFailedLogin, setHasFailedLogin] = useState(false);
 
     const clientID = process.env.REACT_APP_DISCORD_APP_CLIENT_ID;
     const redirectURI = frontendUrl;
@@ -16,7 +17,6 @@ const DiscordAuth = (props) => {
         const code = params.get('code');
 
         if (code) {
-            console.log(backendUrl)
             axios.get(`${backendUrl}/api/discordAuth/exchange`, {
                 withCredentials: true,
                 params: {
@@ -31,32 +31,18 @@ const DiscordAuth = (props) => {
                             if (props.onLogin) {
                                 props.onLogin();
                                 getUserInfo();
+                                setHasFailedLogin(false);
                             }
                         } else {
+                            setHasFailedLogin(true);
                             console.log('User not authenticated')
                         }
                     }
                 })
                 .catch(error => {
-                    console.error('Failed to fetch user data:', error);
+                    console.error('Failed to fetch user data:', error.message);
                 });
         }
-    }
-
-    const checkAuth = () => {
-        axios.get(`${backendUrl}/api/discordAuth/check-auth`, { withCredentials: true })
-            .then(response => {
-                const data = response.data;
-                if (data.isAuthenticated) {
-                    props.onLogin()
-                    getUserInfo()
-
-                    return true
-                }
-            })
-            .catch(error => {
-                console.error('Error checking authentication:', error);
-            });
     }
 
     const getUserInfo = () => {
@@ -85,8 +71,24 @@ const DiscordAuth = (props) => {
 
 
     useEffect(() => {
+        const checkAuth = () => {
+            axios.get(`${backendUrl}/api/discordAuth/check-auth`, { withCredentials: true })
+                .then(response => {
+                    const data = response.data;
+                    if (data.isAuthenticated) {
+                        props.onLogin()
+                        getUserInfo()
+
+                        return true
+                    }
+                })
+                .catch(error => {
+                    console.error('Error checking authentication:', error.message);
+                });
+        }
+
         checkAuth()
-    }, []);
+    }, [props]);
 
     return (
         <div>
@@ -99,10 +101,40 @@ const DiscordAuth = (props) => {
                         </button>
                     </div>
                     :
-                    <div className="login-overlay">
-                        <OauthPopup url={url} onCode={handleLogin} onClose={() => {}} width={1000} height={1000}>
-                            Login
+                    <div className="login-overlay" style={{ display: 'flex', flexDirection: 'column' }}>
+                        <OauthPopup url={url} onCode={handleLogin} onClose={() => { }} width={1000} height={1000}>
+                            <div style={{ textAlign: 'center' }}>
+                                Login
+                            </div>
                         </OauthPopup>
+
+                        <div style={{ textAlign: 'center' }}>
+                            {hasFailedLogin && <span className="error">Not whitelisted, contact admin</span>}
+
+                            {
+                                // DEBUG MODE
+                                // TODO: Remove this before production
+
+                                // Discord User ID refers to a unique identifier for a user, this can be obtained
+                                // via Discord developer mode. This is not the same as the username or nickname.
+                                process.env.REACT_APP_DEBUG_MODE &&
+                                <span className="error">
+                                    <br />
+                                    <input name="discord-id" type="text" placeholder="Discord User ID" />
+                                    <button onClick={async () => {
+                                        await axios.get(`${backendUrl}/api/discordAuth/add-to-whitelist`, {
+                                            withCredentials: true,
+                                            params: {
+                                                discordId: document.getElementsByName('discord-id')[0].value
+                                            }
+                                        })
+                                    }}>
+                                        DEBUG: Add user ID to whitelist
+                                    </button>
+                                </span>
+                            }
+
+                        </div>
                     </div>
             }
         </div>

092c4a14e1c171e891beed18756d8a6a000e91c9
roccomilicic <roccomilicic2@gmail.com>
2023-09-14 23:24:42 +1200

    Added option to add labels, doesnt save on reopen

--- frontend/src/components/Kanban/CardOverlay.jsx
+++ frontend/src/components/Kanban/CardOverlay.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import initialData from "./initialData";
+import { SketchPicker } from 'react-color';
 
 function CardOverlay({
   task,
@@ -9,6 +10,11 @@ function CardOverlay({
   taskDescription,
 }) {
   const [description, setDescription] = useState(taskDescription || "");
+  const [newLabel, setNewLabel] = useState("");
+  const [labelColor, setLabelColor] = useState("#ffffff");
+  const [labels, setLabels] = useState([]);
+  const [isLabelInputVisible, setIsLabelInputVisible] = useState(false);
+  const [isColorPickerVisible, setIsColorPickerVisible] = useState(false);
 
   const handleDescriptionChange = (e) => {
     setDescription(e.target.value);
@@ -23,6 +29,22 @@ function CardOverlay({
     setDescription(taskDescription || "");
   };
 
+  const toggleLabelInput = () => {
+    setIsLabelInputVisible(!isLabelInputVisible);
+    setNewLabel("");
+  };
+
+  const toggleColorPicker = () => {
+    setIsColorPickerVisible(!isColorPickerVisible);
+  };
+
+  const createNewLabel = () => {
+    if (newLabel) {
+      setLabels([...labels, { text: newLabel, color: labelColor }]);
+    }
+    toggleLabelInput();
+  };
+
   return (
     <div className="card-overlay">
       <div className="overlay-content">
@@ -35,18 +57,64 @@ function CardOverlay({
         <div className="labels">
           <h5 className="labels-header">
             Labels
-            <button className="add-labels-btn">+</button>
+            {isLabelInputVisible ? (
+              <div className="label-input-container">
+                <input
+                  type="text"
+                  placeholder="Enter label text"
+                  value={newLabel}
+                  onChange={(e) => setNewLabel(e.target.value)}
+                  className="create-label-input"
+                />
+                <button
+                  className="change-color-btn"
+                  onClick={toggleColorPicker}
+                >
+                  <img src={require('./pick-color.png')} alt="Edit" style={{ width: '18px', height: '18px' }} />
+                </button>
+                <button onClick={createNewLabel} className="create-label-btn">
+                  Create Label
+                </button>   
+                <div className="color-picker-container">
+                  {isColorPickerVisible && (
+                    <div className="color-picker-popover">
+                      <SketchPicker
+                        color={labelColor}
+                        onChange={(color) => setLabelColor(color.hex)}
+                        disableAlpha={true}
+                        presetColors={[]}
+                      />
+                    </div>
+                  )}
+                </div>
+              </div>
+            ) : (
+              <button className="add-labels-btn" onClick={toggleLabelInput}>
+                +
+              </button>
+            )}
           </h5>
+          <div className="labels-list">
+            {labels.map((label, index) => (
+              <span
+                key={index}
+                className="label-text"
+                style={{ backgroundColor: label.color }}
+              >
+                {label.text}
+              </span>
+            ))}
+          </div>
         </div>
         <div className="description">
           <h5 className="description-header">Description</h5>
           <input
             type="text"
-            className="description-input"
             value={description}
             onChange={handleDescriptionChange}
+            className="description-input"
           />
-          <div className="input-button-container">
+          <div className="input-button-container">  
             <button
               className="save-description-btn"
               onClick={handleSaveDescription}

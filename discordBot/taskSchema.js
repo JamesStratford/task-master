c2b143125861308df99d25f0a6bbf1d30232b778
Ethancolt <97382593+Ethancolt@users.noreply.github.com>
2023-09-19 19:36:06 +1200

    remove _id

--- discordBot/taskSchema.js
+++ discordBot/taskSchema.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const taskSchema = new mongoose.Schema({
-    _id: ObjectId,
     userId: String,
     description: String,
     priority: String,

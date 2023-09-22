bdfc301101a644b3c406789a6d82176cb12ca7ea
Ethancolt <97382593+Ethancolt@users.noreply.github.com>
2023-09-22 23:02:26 +1200

    createTask now works

--- discordBot/commands/createTasks/createTask.js
+++ discordBot/commands/createTasks/createTask.js
@@ -1,65 +1,69 @@
-const {SlashCommandBuilder} = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
 const axios = require("axios");
 
 module.exports = {
-
-    data: new SlashCommandBuilder()
-        .setName("create-task")
-        .setDescription("Create a new task!")
-        .addStringOption(option =>
-            option
-                .setName("task_name")
-                .setDescription("Enter the name of the task you want to create")
-                .setRequired(true)
-            )
-        .addStringOption(option =>
-            option
-                .setName("column_name")
-                .setDescription("Enter the name of the column you want to add the task to")
-                .setRequired(true)
+  data: new SlashCommandBuilder()
+    .setName("create-task")
+    .setDescription("Create a new task!")
+    .addStringOption((option) =>
+      option
+        .setName("task_name")
+        .setDescription("Enter the name of the task you want to create")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("column_id")
+        .setDescription(
+          "Enter the id of the column you want to add the task to"
         )
-        .addStringOption(option =>
-            option
-                .setName("due_date")
-                .setDescription("Enter the due date of the task you want to create")
-            )
-        .addStringOption(option =>
-            option
-                .setName("task_description")
-                .setDescription("Enter the description of the task you want to create")
-            ),
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("due_date")
+        .setDescription("Enter the due date of the task you want to create")
+    )
+    .addStringOption((option) =>
+      option
+        .setName("task_description")
+        .setDescription("Enter the description of the task you want to create")
+    ),
+
+  async execute(interaction) {
+    // Identify user
+    const userId = interaction.user.id;
+
+    // To be implemented later...
+
+    // let columnName = interaction.options.getString("column_name")
+
+    // const columns = axios.get(`${process.env.SERVER_ORIGIN}/api/kanban/get-columns`)
+
+    // console.log(`Columns: ${columns}`);
+
+    // const columnId = columns.find(column => column.title === columnName).id
+
+
     
-    async execute(interaction) {
+    // Create a new task to be added to the database
 
-        // Identify user
-        const userId = interaction.user.id;
+    let newCard = {
+      taskId: `task-${Date.now()}`,
+      content: interaction.options.getString("task_name"),
+      due_date: interaction.options.getString("due_date") ?? "",
+      description: interaction.options.getString("task_description") ?? "",
+      labels: [],
+      nextTaskId: "",
+    };
 
-        // Create a new task to be added to the database
+    // Add task to database
 
-        let columnName = interaction.options.getString("column_name")
+    axios.post(`${process.env.SERVER_ORIGIN}/api/kanban/add-task`, {
+      columnId: interaction.options.getString("column_id"),
+      newCard: newCard,
+    });
 
-        const columns = axios.get(`${process.env.SERVER_ORIGIN}/api/kanban/get-columns`)
-
-        const columnId = columns.find(column => column.title === columnName).id
-        
-        let task = {
-            taskId: "1",
-            content: interaction.options.getString("task_name"),
-            due_date: interaction.options.getString("due_date") ?? "",
-            description: interaction.options.getString("task_description") ?? "",
-            labels: [],
-            nextTaskId: ""
-        }
-
-        // Add task to database
-
-        // Waiting for MongoDB integration
-
-        axios.post(`${process.env.SERVER_ORIGIN}/api/kanban/add-task`, {
-            columnId,
-            task
-        })
-
-    }
-
+    await interaction.reply(`Your task ${newCard.content} has been created!`);
+  },
 };

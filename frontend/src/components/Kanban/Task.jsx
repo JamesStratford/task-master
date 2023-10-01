c24467b26e7e54748fd4ae713b90fbcb58b3f353
roccomilicic <roccomilicic2@gmail.com>
2023-10-02 00:08:46 +1300

    Labels now display on cards

--- frontend/src/components/Kanban/Task.jsx
+++ frontend/src/components/Kanban/Task.jsx
@@ -19,52 +19,74 @@ const Task = ({
             {...provided.dragHandleProps}
             onDoubleClick={() => setEditingTaskId(task.taskId)}
         >
-            {isEditing === task.taskId ? (
-                <div className="task-content">
-                    <input
-                        type="text"
-                        value={localContent}
-                        onChange={(e) => setLocalContent(e.target.value)}
-                        onBlur={() => updateTaskContents({ ...task, content: localContent })}
-                    />
-                    <div className="button-container">
-                        <button
-                            className="open-button"
-                            onClick={() => openOverlay(task.taskId)}
-                        >
-                            Open Card
-                        </button>
-                        <button
-                            className="remove-button"
-                            onClick={() => removeCard(task.taskId)}
-                        >
-                            Remove Card
-                        </button>
-                        <button
-                            className="save-button"
-                            onClick={() => {
-                                setEditingTaskId(null);
+            <div className="task-content">
+                <div className="task-labels">
+                    {task.labels && task.labels.map((label, index) => (
+                        <span
+                            key={index}
+                            className="task-label"
+                            style={{
+                                backgroundColor: label.color,
+                                borderRadius: "3px",
+                                padding: "2px 5px",
+                                fontSize: "12px", // smaller font size
+                                whiteSpace: "nowrap", // prevent wrapping
+                                display: "inline-block", // make labels stay in line
+                                marginTop: "2px",
+                                marginRight: "2px", // add some space between labels
                             }}
                         >
-                            Save Card
+                            {label.text}
+                        </span>
+                    ))}
+                </div>
+                {isEditing === task.taskId ? (
+                    <div className="editing-content">
+                        <input
+                            type="text"
+                            value={localContent}
+                            onChange={(e) => setLocalContent(e.target.value)}
+                            onBlur={() => updateTaskContents({ ...task, content: localContent })}
+                        />
+                        <div className="button-container">
+                            <button
+                                className="open-button"
+                                onClick={() => openOverlay(task.taskId)}
+                            >
+                                Open Card
+                            </button>
+                            <button
+                                className="remove-button"
+                                onClick={() => removeCard(task.taskId)}
+                            >
+                                Remove Card
+                            </button>
+                            <button
+                                className="save-button"
+                                onClick={() => {
+                                    setEditingTaskId(null);
+                                }}
+                            >
+                                Save Card
+                            </button>
+                        </div>
+                    </div>
+                ) : (
+                    <div className="non-editing-content">
+                        {task.content}
+                        <button
+                            className="edit-button"
+                            onClick={() => setEditingTaskId(task.taskId)}
+                        >
+                            <img
+                                src={require("./edit.png")}
+                                alt="Edit"
+                                style={{ width: "15px", height: "15px" }}
+                            />
                         </button>
                     </div>
-                </div>
-            ) : (
-                <div className="task-content">
-                    {task.content}
-                    <button
-                        className="edit-button"
-                        onClick={() => setEditingTaskId(task.taskId)}
-                    >
-                        <img
-                            src={require("./edit.png")}
-                            alt="Edit"
-                            style={{ width: "15px", height: "15px" }}
-                        />
-                    </button>
-                </div>
-            )}
+                )}
+            </div>
         </div>
     );
 };

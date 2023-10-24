e195ceb9f9a0e7a02f95f7b3b695a3a19889a2a0
TreyUniWork <xcc7438@autuni.ac.nz>
2023-10-24 16:56:13 +1300

    Fixed checkAttendance bugs.

--- discordBot/commands/misc/checkAttendance.js
+++ discordBot/commands/misc/checkAttendance.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, MessageEmbed } = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
 const axios = require("axios");
 
 module.exports = {
@@ -8,26 +8,32 @@ module.exports = {
 
   async execute(interaction) {
     // Check if the member is in a voice channel
-    const voiceChannel = interaction.member.voice.channel;
+    const member = interaction.member;
+    const voiceChannel = member.voice.channel;
 
     if (!voiceChannel) {
       await interaction.reply('You must be in a voice channel to use this command.');
       return;
     }
 
-    let attendees = voiceChannel.members.map(member => member.id);
-    
+    // Fetch the Discord display names of attendees instead of their IDs
+    let attendeeNames = voiceChannel.members.map(member => member.displayName);
+
+    // Format the current date to a readable string: e.g., "Oct 24, 2023"
+    const currentDate = new Date();
+    const formattedDate = `${currentDate.toLocaleString('default', { month: 'short' })} ${currentDate.getDate()}, ${currentDate.getFullYear()}`;
+
     let meetingColumnId;
 
-    // Check if the "meeting" column exists
+    // Check if the "Meeting" column exists
     try {
-      const columnsResponse = await axios.get(`${process.env.SERVER_ORIGIN}/api/kanban/getColumns`);
-      const meetingColumn = columnsResponse.data.find(col => col.title === "meeting");
+      const columnsResponse = await axios.get(`${process.env.SERVER_ORIGIN}/api/kanban/get-columns`);
+      const meetingColumn = columnsResponse.data.find(col => col.title === "Meetings");
 
       if (!meetingColumn) {
         // Create the column if it doesn't exist
         const newColumnData = {
-          title: "meeting",
+          title: "Meetings",
           tasks: [],
           nextColumnId: null // assuming it's added to the end
         };
@@ -38,18 +44,18 @@ module.exports = {
         meetingColumnId = meetingColumn.id;
       }
     } catch (error) {
-      console.error("Error fetching or creating the meeting column:", error);
-      await interaction.reply({ content: "Error fetching or creating the meeting column. Please try again." });
+      console.error("Error fetching or creating the Meetings column:", error);
+      await interaction.reply("Error fetching or creating the Meetings column. Please try again.");
       return;
     }
 
-    // Create the task and assign it to the "meeting" column
+    // Create the task and assign it to the "Meeting" column
     const newCard = {
       taskId: `task-${Date.now()}`,
-      content: `Meeting: ${attendees.join(', ')}`,
-      startDate: new Date().toISOString(),
-      dueDate: "", 
-      description: "", 
+      content: `Meeting on: ${formattedDate}`,
+      startDate: "", 
+      dueDate: "",
+      description: `Attendees: ${attendeeNames.join(', ')}`,  // Moved attendees info to the description
       labels: [], 
       nextTaskId: null, 
       assignedUser: "" 
@@ -58,21 +64,14 @@ module.exports = {
     try {
       await axios.post(`${process.env.SERVER_ORIGIN}/api/kanban/add-task`, { newCard: newCard, columnId: meetingColumnId });
       
-      const successEmbed = new MessageEmbed()
-        .setColor("#00FF00")
-        .setTitle("Attendance Checked")
-        .setDescription(`Meeting attendees: ${attendees.join(', ')} have been added as a task in the 'meeting' column!`);
-      
-      await interaction.reply({ embeds: [successEmbed] });
+      if (attendeeNames.length === 1) {
+        await interaction.reply(`The Meeting attendee on ${formattedDate}: ${attendeeNames.join(', ')} has been added as a task in the 'Meetings' column!`);
+      } else {
+        await interaction.reply(`Meeting attendees for ${formattedDate}: ${attendeeNames.join(', ')} have been added as a task in the 'Meetings' column!`);
+      }
     } catch(error) {
       console.error("Error adding task:", error);
-      
-      const errorEmbed = new MessageEmbed()
-        .setColor("#FF0000")
-        .setTitle("Error")
-        .setDescription("There was an error adding the task to the 'meeting' column. Please try again.");
-
-      await interaction.reply({ embeds: [errorEmbed] });
+      await interaction.reply("There was an error adding the task to the 'Meetings' column. Please try again.");
     }
   },
 };
